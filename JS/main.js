e38e5c365b72048c3a35dc92ab56b0ba181d95dd
trantrung2212 <131967381+trantrung2212@users.noreply.github.com>
2024-11-22 00:31:10 +0700

    Create main.js

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBjZlfSat-d3MKTgHMgnFo4-hCDv-wTgkw",
+    authDomain: "namlys.firebaseapp.com",
+    databaseURL: "https://namlys-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "namlys",
+    storageBucket: "namlys.firebasestorage.app",
+    messagingSenderId: "573398370432",
+    appId: "1:573398370432:web:5195b26650df2cf0fffe65"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
